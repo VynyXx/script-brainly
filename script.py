28fc35eff9c585b0c30c6e72bb5914061e1b521e
VynyXx <vinicius.pereiras@outlook.com>
2021-10-07 23:37:53 -0300

    1.1

--- script.py
+++ script.py
@@ -2,21 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 url = str(input("Digite a URL: "))
+print("\n")
 page = requests.get(url)
 page.encoding = 'utf-8'
 soup = BeautifulSoup(page.text, 'html.parser')
 
 posts = soup.find_all(class_="sg-text sg-text--break-words brn-rich-content js-answer-content")
-text = str(posts)
 
-print(text.replace("<div class=\"sg-text sg-text--break-words brn-rich-content js-answer-content\" data-test=\"answer-box-text\">", "")
-.replace("<p>", " ")
-.replace("</p>", " ")
-.replace("<strong>", " ")
-.replace("</strong>", " ")
-.replace("<em>", " ")
-.replace("</em>", " ")
-.replace("</div>", " ")
-.replace("<>", " ")
-.replace("</>", " ")
-.replace(",", " "))
+list = []
+
+for post in posts:
+    list.append(post.text)
+
+for each in list:
+    print(each)
