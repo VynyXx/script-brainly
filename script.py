f8b7b1976c9ab533af823b445176233617eeddb5
Vynyx <vinicius.pereiras@outlook.com>
2021-10-08 14:27:30 -0300

    1.3

--- script.py
+++ script.py
@@ -1,20 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
-url = str(input("Digite a URL: "))
-print("\n")
-page = requests.get(url)
-page.encoding = 'utf-8'
-soup = BeautifulSoup(page.text, 'html.parser')
+while True:
+    url = str(input("Digite a URL: "))
+    print()
+    page = requests.get(url)
+    page.encoding = 'utf-8'
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-posts = soup.find_all(class_="sg-text sg-text--break-words brn-rich-content js-answer-content")
+    posts = soup.find_all(class_="sg-text sg-text--break-words brn-rich-content js-answer-content")
 
-list = []
+    list = []
 
-for post in posts:
-    list.append(post.text)
+    for post in posts:
+        list.append(post.text)
 
-i = 1
-for each in list:
-    print("Resposta(%d): " % i, each)
-    i += 1
+    i = 1
+    for each in list:
+        print("Resposta(%d): " % i, each)
+        i += 1
+
+    q = input(str("[1] Tentar de novo\n[2] Sair\n"))
+
+    if q == "2":
+        break
+    else:
+        os.system('cls') or None
